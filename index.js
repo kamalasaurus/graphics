134912331352d679b2ef14dca527d8f67034f778
kamalasaurus <kamalasaurus@gmail.com>
2024-09-04 19:42:14 -0400

    theoretically a canvas

---
+++ index.js
@@ -0,0 +1,11 @@
+void function() {
+  const canvas = document.createElement('canvas');
+  const ctx = canvas.getContext('webgl2');
+
+  canvas.width = 800;
+  canvas.height = 600;
+
+  ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+
+  document.body.appendChild(canvas);
+}();
