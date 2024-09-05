f6a8fda50e9d2348b11da5810e3e34384e2a9e50
kamalasaurus <kamalasaurus@gmail.com>
2024-09-04 20:19:10 -0400

    rotating clear color

--- index.js
+++ index.js
@@ -5,10 +5,13 @@ void function() {
   canvas.width = 800;
   canvas.height = 600;
 
-  window.requestAnimationFrame(() => {
-    ctx.clearColor(0.0, 0.0, 0.0, 1.0);
+  function render() {
+    let [r, g, b] = [Math.random(), Math.random(), Math.random()];
+    ctx.clearColor(r, g, b, 1.0);
     ctx.clear(ctx.COLOR_BUFFER_BIT);
-  })
+  }
+
+  setInterval(render, 500);
 
   document.body.appendChild(canvas);
 }();
